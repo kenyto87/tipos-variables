e99e22ff030aa487372644536670f37da3e428e4
Francisco David <35648594+kenyto87@users.noreply.github.com>
2023-01-22 04:33:26 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const nombre = "Francisco";
+const edad = 39;
+const desarrollador = true;
+const fechaDeNacimiento = new Date(1983, 0, 4);
+const libroFavorito = {
+  titulo: "El capitán veneno",
+  autor: "Pedro Antonio de Alarcón",
+  fecha: "20 de septiembre de 1881",
+  url: "https://biblioteca.org.ar/libros/70235.pdf",
+};
